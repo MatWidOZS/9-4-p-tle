d5b8c6bfc5da26e78d3f4140742c00183dd59c93
Mateusz Widerski <mat.widerski@gmail.com>
2018-03-05 15:54:25 +0100

    Add task for ambitious

--- js/script.js
+++ js/script.js
@@ -10,8 +10,39 @@ function drawTree(treeHeight) {
 	}
 }
 
-drawTree(1);
+drawTree(3);
 
 drawTree(5);
 
 drawTree(10);
+
+
+//=================================
+
+console.log('Dla ambitnych:');
+
+function drawBigTree(h) {
+	for (var i = 0 ; i <= h ; i++) {
+		var star = '';
+
+		for (var s = h - i ; s > 0 ; s--) {
+			star += ' ';
+		}
+
+		for (var g = 0 ; g < i ; g++) {
+			if (g == 0) {
+				star += '*';
+			} else {
+				star += '**';
+			}
+		}
+
+		console.log(star);
+	}
+}
+
+drawBigTree(3);
+
+drawBigTree(5);
+
+drawBigTree(10);
